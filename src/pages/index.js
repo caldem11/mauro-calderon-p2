f7c4435b9983b4d67d3c29ace3654a7f0f7fe769
LinuxTest1 <caldem11@gmatc.matc.edu>
2019-03-28 14:36:51 -0500

    created todo list element and todo items

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Todo from "../components/TodoList"
 
 const IndexPage = () => (
   <Layout>
@@ -11,6 +12,9 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <Todo>
+      
+    </Todo>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
